45e6c0acf04aae653dbb9e8c0ec76457e351c523
wenjoon <hugokim10@gmail.com>
2026-09-15 22:47:54 -0700

    initial file

--- LimitedRegisterMachine.c
+++ LimitedRegisterMachine.c
@@ -7,39 +7,42 @@
 //I have an early copy for a class but am not supposed to share it because its in-the-works
 //however it is really helpful for understanding the concepts (as is MATH 136 at berkeley but I assume thats not accessible everyone)
 
-#define ARRAYSIZE(a) (sizeof(a) / sizeof(a[0]))
-#define LISTARRAY(type, name) \
-    typedef struct name##List { \
-        type* entries; \
-        int used; \
-        int size; \
-    } name; \
-    void name##Init(name* L) { \
-        L->size = 1; \
-        L->used = 0; \
-        L->entries = malloc(sizeof(type)); \
-    } \
-    void name##Add(name* L, type a) { \
-        if (L->used == L->size) { \
-            L->size *= 2; \
-            L->entries = realloc(L->entries, sizeof(type) * L->size); \
-        } \
-        L->entries[L->used] = a; \
-        L->used += 1; \
-    } \
-    void name##ArrayAdd(name* L, type a[], unsigned int size) {\
-        for (int i = 0; i < size; i += 1) {\
-            name##Add(L, a[i]);\
-        }\
-    }\
+//sequences are ordered lists of natural numbers, N = {0, 1, 2, ...}. 
+//As the sequences we deal with will be theorhetically infinite, with non-defined indices defaulting to 0, we will represent them with list-array
 
-#define LA(name, LAname)\
-    LAname name;\
-    LAname##Init(&name);\
+typedef struct SequenceList { 
+        unsigned int* entries; 
+        int used; 
+        int size; 
+} Sequence; 
 
-LISTARRAY(unsigned int, Sequence);
+void SequenceInit(Sequence* L) { 
+    L->size = 1; 
+    L->used = 0; 
+    L->entries = malloc(sizeof(unsigned int)); 
+} 
 
-//Implementing helper functions to build up to a universal function, F(m, n): N^2 -> N, m, n in N using codes. For now it is mostly an exercise as the 32 bit integer limit is reached quickly via encoding sequences being a lot of high prime powers.
+void SequenceAdd(Sequence* L, unsigned int a) { 
+    if (L->used == L->size) { 
+        L->size *= 2; 
+        L->entries = realloc(L->entries, sizeof(unsigned int) * L->size); 
+    } 
+    L->entries[L->used] = a; 
+    L->used += 1; 
+} 
+
+void SequenceArrayAdd(Sequence* L, unsigned int a[], unsigned int size) {
+    for (int i = 0; i < size; i += 1) {
+        SequenceAdd(L, a[i]);
+    }
+}
+
+#define SeqMake(name)\
+    Sequence name;\
+    SequenceInit(&name);\
+
+//Implementing helper functions to build up to a universal function, F(m, n): N^2 -> N, m, n in N using codes. 
+//For now it is mostly an exercise as the 32 bit integer limit is reached quickly via encoding sequences being a lot of high prime powers.
 
 bool IsPrime(unsigned int n) {
     //printf("IsPrime is run");
@@ -52,6 +55,7 @@ bool IsPrime(unsigned int n) {
     }
     return true;
 }
+
 unsigned int NthPrime(unsigned int n) {
     //printf("NthPrime is run\n");
     if (n == 0) {return 2;}
@@ -63,14 +67,20 @@ unsigned int NthPrime(unsigned int n) {
         }
     }
 }
+
 //code of a sequence (in math notated as #seq) is the product of the (value at index n + 1)th power of the nth prime for index 0 through lh(seq)-1.
 //Example: #[1, 2, 3] = 2^(1+1) * 3^(2+1) * 5^(3+1) (with 2, 3, and 5 being the 0th, 1st, and 2nd primes). The +1 in the power is to count indices with value 0 (since entries are in N = {0, 1, ...} 
+
 unsigned int CodeOf(Sequence* seq) {
     unsigned int code = 1;
+
     //printf("CodeOf is run\n");
+
     for (int i = 0; i < seq->used; i += 1) {
         code = code * (unsigned int) powf(NthPrime(i), seq->entries[i] + 1);
+
         //printf("the %i th prime is %u. size of seq.used is %i\n", i, NthPrime(i), seq->used);
+
     }
     return code;
 }
@@ -86,67 +96,108 @@ void PrintSeq(Sequence* seq) {
 //Reconsidering removing limited register machine struct as the machine is more of a process but idk.
 //anywho, URMRun takes in a sequence of instructions and recursively iterates through them until it halts, then returns whatevers at index 0 of the register.
 //There are 4 basic functions:
-// 1. zero instruction: Z(n) tells the machine to change R_n (register n, ie index n of the registers) to a 0
-// 2. successor instruction: S(n) tells the machine to increment R_n by 1
-// 3. transfer instruction: T(m, n) tells the machine to change R_n to the entry at R_m
-// 4. jump instruction: J(m, n, q) tells the machine to move to R_q if R_m = R_n, otherwise to increment the current instruction by 1
+//      1. zero instruction: Z(n) tells the machine to change R_n (register n, ie index n of the registers) to a 0
+//      2. successor instruction: S(n) tells the machine to increment R_n by 1
+//      3. transfer instruction: T(m, n) tells the machine to change R_n to the entry at R_m
+//      4. jump instruction: J(m, n, q) tells the machine to move to R_q if R_m = R_n, otherwise to increment the current instruction by 1
 //Given these numberings, when making a sequence of these instructions, Z(n) -> <1, n>, T(m, n) -> <3, m, n>, etc.
 
-typedef struct LimitedRegisterMachine {
+typedef struct UnlimitedRegisterMachine {
     Sequence* R; //registers. array list of registers. think of it as the ram
     Sequence* I; //Instructions. sequence form of instruction list. think of it as the c code run on the cpu on a single thread
     unsigned int Ci; //current instruction
-} LRM;
-//In order to iterate through these instructions which take up different chunks of the sequence, we must define a few helper functions
-//machine is of type lrm
-#define INavigation(machine)\
-    void machine##INavToFrom(unsigned int n, unsigned int index) {\
-        if\
-    }\
+} URM;
 
-#define Instructions(machine)\
-    void machine##RegBound(unsigned int n) {\
-        if (n > machine.R.used){\
-            for (int i = machine.R.used; machine.R.used < n; i += 1) {\
-                SequenceAdd(*machine.R, 0);\
-            }\
-        }\
-    }\
-    void machine##Z(unsigned int n) {\
-        machine##RegBound(n);\
-        *machine.R[n] = 0;\
-        machine.Ci += 1;\
-    }\
-    void machine##S(unsigned int n) {\
-        machine##RegBound(n);\
-        *machine.R[n] += 1;\
-        machine.Ci += 1;\
-    }\
-    void machine##T(unsigned int m, unsigned int n) {\
-        machine##RegBound(n);\
-        machine##RegBound(m);\
-        *machine.R[n] = *machine.R[m]\
-        machine.Ci += 1;\
-    }\
-    void machine##J(unsigned int m, unsigned int n, unsigned int q) {\
-        machine##RegBound(n);\
-        machine##RegBound(m);\
-        if (*machine.R[n] == *machine.R[m]) {\
-            machine.Ci = q;\
-        }\
-        else {machine.Ci += 1}\
-    }\
+//What is regbound? In order to model a URM without unlimited memory, we must make a few concessions. 
+//In this case, lest a register be specified, it will be assumed to be 0. 
+//If we want to operate on such a register but it is out of our sequence's bounds, we can add it and and all fill in all registers before it
+//I am considering a dictionary-structure to avoid adding these extra registers that are all zeroes, but assuming programs try and use registers efficiently, this structure should suffice
 
-unsigned int URMRun(LRM lrm) {
-    if (lrm.Ci > lrm.I->used) {
-        return lrm.R->entries[0];
+void URMRegBound(URM machine, unsigned int n) {
+    if (n > machine.R->used){
+        for (int i = machine.R->used; machine.R->used < n; i += 1) {
+            SequenceAdd(machine.R, 0);
+        }
     }
+}
+
+//In order to iterate through in an uncomplicated fashion, we will append chunks of 4 numbers for each instruction (as the larges instruction, jumps, take in 3 inputs),
+//In reality, the instruction sequence would contain instruction "chunks" of differing length. However, as that is not the focus, it is on hold.
+//machine is of type lrm
+
+void Zeroer(URM machine, unsigned int (*n)[3]) {
+    URMRegBound(machine, *n[0]);
+    machine.R->entries[*n[0]] = 0;
+    machine.Ci += 1;
+}
+
+void Successor(URM machine, unsigned int (*n)[3]) {
+    URMRegBound(machine, *n[0]);
+    machine.R->entries[*n[0]] += 1;
+    machine.Ci += 1;
+}
+
+void Transfer(URM machine, unsigned int (*n)[3]) {
+    URMRegBound(machine, *n[0]);
+    URMRegBound(machine, *n[1]);
+    machine.R->entries[*n[0]] = machine.R->entries[*n[1]];
+    machine.Ci += 1;
+}
+
+void Jumper(URM machine, unsigned int (*n)[3]) {
+    URMRegBound(machine, *n[0]);
+    URMRegBound(machine, *n[1]);
+    if (machine.R->entries[*n[1]] == machine.R->entries[*n[0]]) {
+        machine.Ci = *n[2];
+    }
+    else {machine.Ci += 1;}
+}
+
+void (*funk[4])(URM, unsigned int (*)[3]) = {Zeroer, Successor, Transfer, Jumper};
+
+unsigned int pholder[4];
+
+//These macros automatically add instructions to the instruction sequence!
+//I will leave a a section down in the main function such that you may try programming a URM!
+//Note that "machine" is of time URM, and pholder will only store the last instruction added (it is a place-holder, thus pholder)
+#define Z(n, machine)\
+    pholder = {1, n, 0, 0};\
+    SequenceArrayAdd(machine.I, pholder, 4);\
+
+#define S(n, machine)\
+    pholder = {2, n, 0, 0};\
+    SequenceArrayAdd(machine.I, pholder, 4);\
+
+#define T(m, n, machine)\
+    pholder = {3, m, n, 0};\
+    SequenceArrayAdd(machine.I, pholder, 4);\
+
+#define J(m, n, q, machine)\
+    pholder = {4, m, n, q};\
+    SequenceArrayAdd(machine.I, pholder, 4);\
+
+//Now, how to run this URM? Recursively! Because the behavior of a given instruction on a specific sequence of registers is determined, 
+//we can think of a specific register "input", the same instructions, and a "current instruction" (as denoted by ci) as all the information we need to determine a specific state of the URM
+//A urm whose ci is beyond the index of the last instruction "halts" (as there is nothing left to do), and so the program returns the number in register 0
+unsigned int ciIndex;
+unsigned int inputs[3];
+
+unsigned int URMRun(URM machine) {
+    if (machine.Ci > machine.I->used) {
+        return machine.R->entries[0];
+    }
+    else{
+        ciIndex = machine.Ci * 4;
+        inputs[0] = machine.I->entries[ciIndex + 1];
+        inputs[1] = machine.I->entries[ciIndex + 2];
+        inputs[2] = machine.I->entries[ciIndex + 3];
     
+        funk[machine.I->entries[ciIndex] - 1](machine, &inputs);
+        PrintSeq(machine.R);
+        return URMRun(machine);
+    }
 }
 
 void main() {
-    LA(A, Sequence);
-    unsigned int toAdd[] = {0, 1};
-    SequenceArrayAdd(&A,  toAdd, ARRAYSIZE(toAdd));
-
+    
 }
